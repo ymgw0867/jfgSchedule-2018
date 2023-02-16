f02dd622da46ee3dd6a2d206bd059f5cafa13878
ymgw0867 <kyamagiwa@gmail.com>
2023-02-16 12:11:53 +0900

    20230216

---
+++ jfgSchedule/ClsEastEng.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jfgSchedule
+{
+    public class ClsEastEng
+    {
+        public double カード番号 { get; set; }
+        public string 氏名 { get; set; }
+        public 会員稼働予定 会員稼働予定 { get; set;}
+    }
+}
