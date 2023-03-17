a0b6f938d0f688c0e47fa8e46bedbc3918fee70c
ymgw0867 <kyamagiwa@gmail.com>
2023-03-17 13:07:52 +0900

    20230317 ブランチ作成

--- jfgSchedule/ClsEastEng.cs
+++ jfgSchedule/ClsEastEng.cs
@@ -10,6 +10,15 @@ namespace jfgSchedule
     {
         public double カード番号 { get; set; }
         public string 氏名 { get; set; }
-        public 会員稼働予定 会員稼働予定 { get; set;}
+        public string 携帯電話 { get; set; }
+        public string 生まれ年 { get; set; }
+        public string 住所都道府県 { get; set; }
+        public string 住所市区 { get; set; }
+        public string メールアドレス { get; set; }
+        public string 他言語ライセンス { get; set; }
+        public string JFG加入年 { get; set; }
+        public int JFG稼働日数1 { get; set; }
+        public int JFG稼働日数2 { get; set; }
+        public 会員稼働予定 会員稼働予定 { get; set; }
     }
 }
